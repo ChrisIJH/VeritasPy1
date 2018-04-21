7489ad79deea99302ec74359c45299f628b4c711
Research Env <research@strat.local>
2018-04-21 11:39:48 -0400

    asdf

---
+++ ch07_Selection/07.py
@@ -0,0 +1,15 @@
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+
+def is_odd(n):
+	if n %2 !=0:
+		return True
+	else:
+		return False
+
+if __name__=='__main__':
+	print (is_even(4))
